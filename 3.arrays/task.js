d52f49f2961b8ebab659ec7d2a9fc113d69b9f2e
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-04-06 06:39:57 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,36 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => {
+      return value === arr2[index];
+    })
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (
+    users.length === 0 ||
+    !users
+      .map((value) => {
+        return value.gender;
+      })
+      .includes(gender)
+  ) {
+    return 0;
+  }
+
+  let resultArrLength = 0;
+  let result = users
+    .filter((value) => {
+      return value.gender === gender;
+    })
+    .map((value, index, array) => {
+      resultArrLength = array.length;
+      return value.age;
+    })
+    .reduce((acc, value) => {
+      return acc + value;
+    }, 0);
+
+  return result / resultArrLength;
 }
