e0455f484c3696f75c74007f58515361a1c81f6a
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-04-15 07:56:12 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,70 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    this.alarmCollection.forEach((alarmElem) => {
+      if (alarmElem[time]) {
+        console.warn('Уже присутствует звонок на это же время');
+      }
+    });
+
+    this.alarmCollection.push({
+      callback: callback,
+      time: time,
+      canCall: true,
+    });
+  }
+
+  removeClock(time) {
+    const updateAlarmCollection = this.alarmCollection.filter((alarmElem) => {
+      return alarmElem.time !== time;
+    });
+
+    this.alarmCollection = updateAlarmCollection;
+  }
+
+  getCurrentFormattedTime() {
+    return new Date().toTimeString().slice(0, 5);
+  }
+
+  start() {
+    if (this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.alarmCollection.forEach((alarmElem) => {
+          if (
+            alarmElem.time === this.getCurrentFormattedTime() &&
+            alarmElem.canCall === true
+          ) {
+            alarmElem.canCall = false;
+            return alarmElem.callback();
+          }
+        });
+      }, 1000);
+    }
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach((alarmElem) => {
+      alarmElem.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
+
+
