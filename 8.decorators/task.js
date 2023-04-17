09438a5891d9bb5d05382537b3092c0e27289406
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-04-17 10:31:50 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,47 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  function wrapper(...args) {
+    const hash = md5([...args]);
+    let indexInCache = cache.findIndex((item) => item.hash === hash);
+    if (indexInCache !== -1) {
+      console.log('Из кэша: ' + cache[indexInCache].value);
+      return 'Из кэша: ' + cache[indexInCache].value;
+    }
+    let result = func(...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log('Вычисляем: ' + result);
+    return 'Вычисляем: ' + result;
+  }
+
+  cache = [];
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutID = null;
+  let isTrottled = false;
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
+      func(args);
+      wrapper.count++;
+    }, delay);
+
+    if (!isTrottled) {
+      func(...args);
+      wrapper.count++;
+      isTrottled = true;
+    }
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
