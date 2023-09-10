4f490959cd32508233b8a7e6544f531c92bcb5b0
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-10 09:24:36 +0300

    refactor: change structure Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,3 +1,10 @@
+/**
+ * Функция конструктор формирет объект с данными студента.
+ *
+ * @param {string} name имя студента.
+ * @param {string} gender пол студента.
+ * @param {number} age возраст студента.
+ */
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
@@ -5,16 +12,33 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
+/**
+ * Метод, устанавливает поле subject, присваивает ему subjectName.
+ *
+ * @param {string} subjectName наименование предмета.
+ *
+ */
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 };
 
+/**
+ * Метод, добавляет студенту несколько оценок.
+ *
+ * @param {object} marks последовательность значений оценок.
+ *
+ */
 Student.prototype.addMarks = function (...marks) {
   if (this.marks) {
     this.marks.push(...marks);
   }
 };
 
+/**
+ * Метод, возвращает среднее арифметическое оценок студента.
+ *
+ * @return {number} среднее арифметическое оценок студента
+ */
 Student.prototype.getAverage = function () {
   if (!this.marks || this.marks.length === 0) {
     return 0;
@@ -26,6 +50,12 @@ Student.prototype.getAverage = function () {
   );
 };
 
+/**
+ * Метод, исключает студента из учебного процесса.
+ *
+ * @param {string} reason причина исключения.
+ *
+ */
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
