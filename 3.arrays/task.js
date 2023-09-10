507b010d70e69b655ecf58c8109b1661fb683288
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-10 07:35:02 +0300

    refactor: change structure Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,3 +1,10 @@
+/**
+ * Сравнивает значения двух массивов, возвращает true или false в зависимости от результатов сравнения.
+ *
+ * @param {object} arr1 массив значений.
+ * @param {object} arr2 массив значений.
+ * @return {boolean} итог сравнения элементов массива
+ */
 function compareArrays(arr1, arr2) {
   return (
     arr1.length === arr2.length &&
@@ -5,6 +12,13 @@ function compareArrays(arr1, arr2) {
   )
 }
 
+/**
+ * Возвращает среднее значение возраста пользователей одного пола.
+ *
+ * @param {object} users массив пользователей.
+ * @param {string} gender пол.
+ * @return {number} среднее значение возраста пользователей одного пола
+ */
 function getUsersNamesInAgeRange(users, gender) {
   let result = users
     .filter((value) => {
