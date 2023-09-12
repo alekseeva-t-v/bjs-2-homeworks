f5954519adb10361fe2d62558db91fc2a111ed11
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-09-12 09:22:49 +0300

    refactor: change structure Async

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,9 +1,19 @@
 class AlarmClock {
+  /**
+   * Конструктор объекта будильник (Создает свойство для хранения коллекции звонков `alarmCollection` и свойство `intervalId` для хранения `id` таймера).
+   *
+   */
   constructor() {
     this.alarmCollection = [];
     this.intervalId = null;
   }
 
+  /**
+   * Добавляет новый звонок в коллекцию существующих.
+   *
+   * @param {string} time Время, когда действие должно запуститься.
+   * @param {object} callback Функция-колбека — действие, которое должно запуститься.
+   */
   addClock(time, callback) {
     if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
@@ -24,6 +34,12 @@ class AlarmClock {
     });
   }
 
+  /**
+   * Удаляет звонки по определённому времени.
+   *
+   * @param {string} time Время, звонка, который следует удалить.
+   *
+   */
   removeClock(time) {
     const updateAlarmCollection = this.alarmCollection.filter((alarmElem) => {
       return alarmElem.time !== time;
@@ -32,10 +48,19 @@ class AlarmClock {
     this.alarmCollection = updateAlarmCollection;
   }
 
+  /**
+   * Возвращает текущее время в строковом формате.
+   *
+   * @return {string} текущее время в строковом формате.
+   */
   getCurrentFormattedTime() {
     return new Date().toTimeString().slice(0, 5);
   }
 
+  /**
+   * Запускает будильник.
+   *
+   */
   start() {
     if (this.intervalId === null) {
       this.intervalId = setInterval(() => {
@@ -52,17 +77,29 @@ class AlarmClock {
     }
   }
 
+  /**
+   * Останавливает выполнение интервала будильника.
+   *
+   */
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
 
+  /**
+   * Сбрасывает возможность запуска всех звонков.
+   *
+   */
   resetAllCalls() {
     this.alarmCollection.forEach((alarmElem) => {
       alarmElem.canCall = true;
     });
   }
 
+  /**
+   * Удаляет все звонки.
+   *
+   */
   clearAlarms() {
     this.stop();
     this.alarmCollection = [];
